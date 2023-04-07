86ebe2e5a9affbe866f2e6705738361eb7052be8
Trenton <tregilbe@uat.edu>
2023-04-07 17:38:46 -0500

    Add files via upload

---
+++ Binary Search Tree/Source.cpp
@@ -0,0 +1,172 @@
+#include<iostream>
+using namespace std;
+
+class BST {
+
+    struct node {
+        int data;
+        node* left;
+        node* right;
+    };
+
+    node* root;
+
+    node* makeEmpty(node* t) {
+        if (t == NULL)
+            return NULL;
+        {
+            makeEmpty(t->left);
+            makeEmpty(t->right);
+            delete t;
+        }
+        return NULL;
+    }
+
+    node* insert(int x, node* t)
+    {
+        if (t == NULL)
+        {
+            t = new node;
+            t->data = x;
+            t->left = t->right = NULL;
+        }
+        else if (x < t->data)
+            t->left = insert(x, t->left);
+        else if (x > t->data)
+            t->right = insert(x, t->right);
+        return t;
+    }
+
+    node* findMin(node* t)
+    {
+        if (t == NULL)
+            return NULL;
+        else if (t->left == NULL)
+            return t;
+        else
+            return findMin(t->left);
+    }
+
+    node* findMax(node* t) {
+        if (t == NULL)
+            return NULL;
+        else if (t->right == NULL) {
+            cout << t->data << endl;
+            return t;
+        }
+        else {
+            return findMax(t->right);
+        }
+    }
+
+    node* remove(int x, node* t) {
+        node* temp;
+        if (t == NULL)
+            return NULL;
+        else if (x < t->data)
+            t->left = remove(x, t->left);
+        else if (x > t->data)
+            t->right = remove(x, t->right);
+        else if (t->left && t->right)
+        {
+            temp = findMin(t->right);
+            t->data = temp->data;
+            t->right = remove(t->data, t->right);
+        }
+        else
+        {
+            temp = t;
+            if (t->left == NULL)
+                t = t->right;
+            else if (t->right == NULL)
+                t = t->left;
+            delete temp;
+        }
+
+        return t;
+    }
+
+    void inorder(node* t) {
+        if (t == NULL)
+            return;
+        inorder(t->left);
+        cout << t->data << " ";
+        inorder(t->right);
+    }
+
+    node* find(node* t, int x) {
+        if (t == NULL)
+            return NULL;
+        else if (x < t->data)
+            return find(t->left, x);
+        else if (x > t->data)
+            return find(t->right, x);
+        else
+            return t;
+    }
+
+public:
+    BST() {
+        root = NULL;
+    }
+
+    ~BST() {
+        root = makeEmpty(root);
+    }
+
+    void insert(int x) {
+        root = insert(x, root);
+    }
+
+    void remove(int x) {
+        root = remove(x, root);
+    }
+
+    void display() {
+        inorder(root);
+        cout << endl;
+    }
+
+    void findMax() {
+        findMax(root);
+    }
+
+    void search(int x) {
+        root = find(root, x);
+    }
+};
+
+int main() {
+    BST t;
+    // Build Tree
+    cout << "Building Tree...." << endl;
+    t.insert(20);
+    t.insert(25);
+    t.insert(15);
+    t.insert(10);
+    t.insert(30);
+    // Display the starting tree
+    cout << "Displaying Base Tree...." << endl;
+    t.display();
+    // Find Max value
+    cout << "Finding Max Value...." << endl;
+    t.findMax();
+    // Remove 20 and display
+    cout << "Removing 20...." << endl;
+    t.remove(20);
+    t.display();
+    // Remove 25 and display
+    cout << "Removing 25...." << endl;
+    t.remove(25);
+    t.display();
+    // Remove 30 and display
+    cout << "Removing 30...." << endl;
+    t.remove(30);
+    t.display();
+    // Find Max value
+    cout << "Finding Max Value...." << endl;
+    t.findMax();
+
+    
+    return 0;
+}
